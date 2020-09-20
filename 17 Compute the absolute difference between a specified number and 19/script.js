1192e90c04cc4479057850f0cf98f4e078076211
Najmul-Islam <najmulislam519@gmail.com>
2020-09-20 07:30:25 +0600

    complete 17no. paractice

---
+++ 17 Compute the absolute difference between a specified number and 19/script.js
@@ -0,0 +1,10 @@
+document.getElementById('btn').addEventListener('click', function () {
+    const inValue = parseInt(document.getElementById('input').value);
+
+    if (inValue => 19) {
+        var result = 19 - inValue;
+    } else {
+        var result = n - 19 * 3;
+    }
+    document.getElementById('result').innerText = 'Result : ' + result;
+})
